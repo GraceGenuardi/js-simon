22d98bb5aa8afb28353c6a5ce1ef1fe57df69f4e
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-13 17:47:22 +0100

    EDIT js part 2-3

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 //1.Visualizzare in pagina 5 numeri casuali con alert.
+    //Usiamo un array per memorizzare i numbers che attiverà il ciclo for
+    let numbers = [];
+    for (let i = 0; i < 5; i++) {
+      let randomNumber = Math.floor(Math.random() * 100);
+      numbers.push(randomNumber);
+      alert(randomNumber);
+    }
 
-//Ciclo for per mostrare i numeri generati randon con math.random, arrotondiamo con math.floor
-for (let i = 0; i < 5; i++) {
-    let randomNumber = Math.floor(Math.random() * 5);
-    alert(randomNumber);
-  }
 
 
 //2.Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
-
-
-
 //3.Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
+    //Scaduti i 30 sec, chiediamo all'utente tramite un'altro ciclo for i numeri visti prima
+setTimeout(function() {
+    let userNumbers = [];
+    for (let i = 0; i < 5; i++) {
+      let userInput = prompt("Inserisci il numero che hai visto prima:");
+      userNumbers.push(parseInt(userInput));
+    }
+
+    //creiamo un array per memorizzare i numeri e altro ciclo for per confrontare i numeri
+    let correctNumbers = [];
+  for (let i = 0; i < 5; i++) {
+    if (numbers.includes(userNumbers[i])) {
+      correctNumbers.push(userNumbers[i]);
+    }
+  }
 
 
-
-//4.Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
