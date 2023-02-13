56abad79f3e4e33a15d826d111050f33c95fe166
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-13 13:22:43 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//1.Visualizzare in pagina 5 numeri casuali con alert.
+
+
+//2.Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
+
+
+
+//3.Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
+
+
+
+//4.Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
